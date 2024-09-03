a62b98de2b1b87c70603a5e856b473948263be9b
melanylibia <elibialany@gmail.com>
2024-09-03 01:14:08 -0400

    feat: update con

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from  flask import Flask,request,render_template,redirect,url_for,flash
 import os
 app = Flask(__name__)
+import pymysql.cursors  
+connection = pymysql.connect(host= os.getenv('DB_HOST'),
+user=    os.getenv("DB_USER"),
+password=os.getenv("DB_PASSWD"),
+database=os.getenv("DB_NAME"),
+cursorclass=pymysql.cursors.DictCursor)  
+app.secret_key = os.getenv('key')
+app.config['SESSION_TYPE'] = 'filesystem'
 
 @app.route('/')
 def hello_world():
