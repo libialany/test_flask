48b9879836233a4bc55536bf7e60770dea9d9587
LibiaMLany <82403984+libialany@users.noreply.github.com>
2024-09-02 23:08:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask
-
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World!' + os.getenv("SAY_HI")
 
 if __name__ == '__main__':
     app.run(port=4983,host='0.0.0.0',debug=True)
